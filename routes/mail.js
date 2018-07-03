8d4b30fa129fbde5799c9db5051b01a310495456
devin <the.devin.carter@outlook.com>
2018-07-03 13:31:15 -0500

    change dependencies

--- routes/mail.js
+++ routes/mail.js
@@ -6,43 +6,45 @@ var graph = require('@microsoft/microsoft-graph-client');
 
 /* GET /mail */
 router.get('/', async function(req, res, next) {
-  let parms = { title: 'Inbox', active: { inbox: true } };
+    let parms = { title: 'Inbox', active: { inbox: true } };
 
-  const accessToken = await authHelper.getAccessToken(req.cookies, res);
-  const userName = req.cookies.graph_user_name;
+    const accessToken = await authHelper.getAccessToken(req.cookies, res);
+    const userName = req.cookies.graph_user_name;
 
-  if (accessToken && userName) {
-    parms.user = userName;
+    if (accessToken && userName) {
+        parms.user = userName;
 
-    // Initialize Graph client
-    const client = graph.Client.init({
-      authProvider: (done) => {
-        done(null, accessToken);
-      }
-    });
+        // Initialize Graph client
+        const client = graph.Client.init({
+            authProvider: (done) => {
+                done(null, accessToken);
+            }
+        });
 
-    try {
-      // Get the 10 newest messages from inbox
-      const result = await client
-      .api('/me/mailfolders/inbox/messages')
-      .top(10)
-      .select('subject,from,receivedDateTime,isRead')
-      .orderby('receivedDateTime DESC')
-      .get();
+        try {
+            // Get first messssge from inbox
+            const result = await client
+                .api('/me/mailfolders/inbox/messages')
+                .top(1)
+                // .select('subject,from,receivedDateTime,isRead')
+                // .orderby('receivedDateTime DESC')
+                .get();
 
-      parms.messages = result.value;
-      res.render('mail', parms);
-    } catch (err) {
-      parms.message = 'Error retrieving messages';
-      parms.error = { status: `${err.code}: ${err.message}` };
-      parms.debug = JSON.stringify(err.body, null, 2);
-      res.render('error', parms);
+            // parms.message = result.value;
+            res.render('mail', parms);
+            console.log(result.value[0].body.content);
+
+        } catch (err) {
+            parms.message = 'Error retrieving messages';
+            parms.error = { status: `${err.code}: ${err.message}` };
+            parms.debug = JSON.stringify(err.body, null, 2);
+            res.render('error', parms);
+        }
+
+    } else {
+        // Redirect to home
+        res.redirect('/');
     }
-    
-  } else {
-    // Redirect to home
-    res.redirect('/');
-  }
 });
 
 module.exports = router;
